69d11ad1cb95ce299c6554036ec96a6b94973ddc
ireshanwaiman94 <irashanmaduranga071@gmail.com>
2025-06-26 13:42:11 +0530

    delete all
    adw

--- wp-content/plugins/astra-sites/inc/lib/zip-ai/sidebar/build/sidebar-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-rich-text'), 'version' => '18b8d82dc013957ef978');
